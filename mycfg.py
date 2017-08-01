8ea59414ba71cca2ce3a7614e7e0cbc263ff7820
Nuno Correia <nc@tfv.pt>
2017-08-01 17:46:25 +0100

    run.bat

--- mycfg.py
+++ mycfg.py
@@ -1,5 +1,5 @@
 c = get_config()
-c.NbConvertApp.notebooks = ["*.ipynb"]
+c.NbConvertApp.notebooks = ["Teste.ipynb"]
 c.NbConvertApp.export_format = 'slides'
 c.SlidesExporter.reveal_url_prefix = './reveal.js'
 c.inlining_css = ''
